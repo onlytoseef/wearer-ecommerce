bf8c1e931eb20311fac1c4a4cdb74bf24a339e9b
onlytoseef <hellotoseef@gmail.com>
2024-12-27 21:13:09 +0500

    added route of track suit

--- src/components/CategoryCarousel/CategoryCarousel.jsx
+++ src/components/CategoryCarousel/CategoryCarousel.jsx
@@ -9,7 +9,7 @@ const categories = [
     title: "Track Suits",
     image:
       "https://plus.unsplash.com/premium_photo-1689371958614-cd30635630ef?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    link: "/upcoming",
+    link: "/productPage",
   },
   {
     id: 2,
