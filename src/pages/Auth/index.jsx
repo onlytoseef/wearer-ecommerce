48c5a4e690f2a7b1781d9d957a62308d3c790c8f
onlytoseef <hellotoseef@gmail.com>
2025-01-07 22:35:51 +0500

    fix auth import

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Login";
-import Register from "./register";
+import Register from "./Register";
 
 export default function Index() {
   return (
