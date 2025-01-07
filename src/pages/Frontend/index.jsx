589ee8602ee21244827d1c9f364ad912528fac6b
onlytoseef <hellotoseef@gmail.com>
2025-01-07 22:28:29 +0500

    fix frontend import

--- src/pages/Frontend/index.jsx
+++ src/pages/Frontend/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./home";
+import Home from "./Home";
 import About from "./About";
 import Categories from "./Categories";
 import NotFound from "./NotFound";
